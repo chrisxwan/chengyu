1948586bf66f315ea935bd213346fcc2d96ba245
Christopher Wan <christopher.wan@yale.edu>
2015-08-11 17:44:52 -0400

    web scrape

--- js/output.js
+++ js/output.js
@@ -1,3 +1,16 @@
 $.get('http://ntireader.org/topic.php?english=Idiom', function (data) {
 	console.log(data);
+	var dom = $.parseHTML(data);
+	var tr = $(dom).find('tr');
+	var num = tr.length;
+	var rand = Math.floor(Math.random() * num) + 1;
+	var outer = $(tr).get(rand);
+	var inner = $(outer).contents();
+	var simplified = $(inner).get(0).innerHTML;
+	var traditional = $(inner).get(1).innerHTML;
+	if(!traditional) {
+		simplified = traditional;
+	}
+	var pinyin = $(inner).get(2).innerHTML;	
+	var definition = $(inner).get(3).innerHTML;
 });
