00f3b7bc36b2732bd096b86765ee817b79ba0dd9
Christopher Wan <christopher.wan@yale.edu>
2015-08-11 18:40:21 -0400

    basic styling

--- js/output.js
+++ js/output.js
@@ -6,11 +6,13 @@ $.get('http://ntireader.org/topic.php?english=Idiom', function (data) {
 	var rand = Math.floor(Math.random() * num) + 1;
 	var outer = $(tr).get(rand);
 	var inner = $(outer).contents();
-	var simplified = $(inner).get(0).innerHTML;
-	var traditional = $(inner).get(1).innerHTML;
+	var simplified = '<p id="simplified">Simplified: ' + $($(inner).get(0)).text() + '</p>';
+	var traditional = '<p id="traditional">Traditional: ' + $($(inner).get(1)).text() + '</p>';
 	if(!traditional) {
 		simplified = traditional;
 	}
-	var pinyin = $(inner).get(2).innerHTML;	
-	var definition = $(inner).get(3).innerHTML;
+	var pinyin = '<p id="pinyin">Pinyin: ' + $(inner).get(2).innerHTML + '</p>';	
+	var definition = '<p id="definition">Definition: ' + $(inner).get(3).innerHTML + '</p>';
+	var appendString = simplified + traditional + pinyin + definition;
+	$('.text').append(appendString);
 });
